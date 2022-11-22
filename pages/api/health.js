958ca9754a5b5202b024c32bd24086b04e9db46e
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2022-11-22 14:40:21 +0100

    adding the project skeleton

---
+++ pages/api/health.js
@@ -0,0 +1,5 @@
+const { StatusCodes, getReasonPhrase } = require('http-status-codes');
+
+module.exports = function handler(req, res) {
+  res.status(StatusCodes.OK).json({ status: getReasonPhrase(StatusCodes.OK) })
+}
---
+++ pages/api/pokemon/[name].js
@@ -0,0 +1,29 @@
+const {
+  StatusCodes,
+  getReasonPhrase,
+} = require("http-status-codes");
+
+const _ = require("lodash");
+const poke = require("../../../utils/pokeapi");
+
+module.exports = async function handler(req, res) {
+  console.log("req. query: ", req.query);
+  const path = `/pokemon-species/${req.query.name}`;
+
+  const response = await poke.api(path);
+
+  const pokemon = {}
+  pokemon.name = _.get(response, "name")
+  pokemon.description =
+    _.chain(response.flavor_text_entries)
+    .find({  language: { name: 'en'}  })
+    .pick('flavor_text')
+    .get('flavor_text')
+    .value()
+  pokemon.habitat = _.get(response, 'habitat.name')
+  pokemon.isLegendary = _.get(response, 'is_legendary')
+
+  res
+    .status(StatusCodes.OK)
+    .json(pokemon);
+};
