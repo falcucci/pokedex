88a5259ecc3b9bf96ecd43a2c86e41b6a0153881
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2022-11-22 17:50:14 +0100

    refactor to get pokemon by name

--- pages/api/pokemon/[name].js
+++ pages/api/pokemon/[name].js
@@ -1,22 +1,10 @@
-const {
-  StatusCodes,
-  getReasonPhrase,
-} = require("http-status-codes");
+const { StatusCodes } = require("http-status-codes");
 
-const _ = require("lodash");
-const poke = require("../../../utils/poke");
+const Pokedex = require("../../../utils/Pokedex");
 
 module.exports = async function handler(req, res) {
-  const path = `/pokemon-species/${req.query.name}`;
-  const response = await poke.api(path);
-  const pokemon = {};
-  pokemon.name = _.get(response, "name");
-  pokemon.description = _.chain(response.flavor_text_entries)
-    .find({ language: { name: "en" } })
-    .pick("flavor_text")
-    .get("flavor_text")
-    .value();
-  pokemon.habitat = _.get(response, "habitat.name");
-  pokemon.isLegendary = _.get(response, "is_legendary");
+  const name = req.query.name;
+  const pokedex = new Pokedex();
+  const pokemon = await pokedex.get({ name });
   res.status(StatusCodes.OK).json(pokemon);
 };
