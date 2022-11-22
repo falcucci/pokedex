e3ff274647033721fd030bb7ddb27ad9253a5759
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2022-11-22 15:58:28 +0100

    adding translations

--- pages/api/pokemon/translated/[name].js
+++ pages/api/pokemon/translated/[name].js
@@ -4,7 +4,15 @@ const {
 } = require("http-status-codes");
 
 const _ = require("lodash");
-const poke = require("../../../../utils/pokeapi");
+const poke = require("../../../../utils/poke");
+const yoda = require("../../../../utils/yoda");
+const shakespare = require("../../../../utils/shakespare");
+
+const pokemon = {
+  habitat: {
+    CAVE: "cave",
+  },
+};
 
 module.exports = async function handler(req, res) {
   const path = `/pokemon-species/${req.query.name}`;
@@ -18,5 +26,14 @@ module.exports = async function handler(req, res) {
     .value();
   pokemon.habitat = _.get(response, "habitat.name");
   pokemon.isLegendary = _.get(response, "is_legendary");
+
+  const tr = await yoda.api({ body: { text: pokemon.description }, method: 'POST' });
+  const isYoda = pokemon.habitat === pokemon.habitat.CAVE || pokemon.isLegendary
+
+  const yodaTr = _.get(tr, 'contents.translated')
+
+  const sha = await shakespare.api({ body: { text: pokemon.description }, method: 'POST' });
+  console.log('sha: ', sha);
+  pokemon.description = isYoda ? yodaTr ? (sha || pokemon.description)
   res.status(StatusCodes.OK).json(pokemon);
 };
